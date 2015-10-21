dd861a861941edeaabb70a33d0334b2502f0ca89
CheeeProject <tony@cheee.com.au>
2015-10-21 14:54:02 +1100

    TestPHP

---
+++ testing.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tonylin
+ * Date: 15-8-27
+ * Time: 下午1:15
+ */
+
+include('header_inc.php');
+?>
+
+    <!--=== Content Part ===-->
+    <div class="container content">
+
+        <link rel="stylesheet" href="/golf_/css/custom.css">
+        <link rel="stylesheet" href="/assets/css/pages/page_404_error.css">
+
+        <div class="row div_404">
+            <div class="col-xs-4 congratulation-face">
+                <img src="/golf_/image/icon/oops.png" alt="smileface" width="90">
+            </div>
+            <div class="col-xs-8">
+                <div class="error-v1">
+                    <span class="register-font-color margin-bottom-20">Oops! This is awkward.</span>
+                    <p class="register-font-color">You are looking for something that doesn't actually exist! /404 NOT FOUND/</p>
+                    <a class="activate-button" href="/index.php">Back to Home Page</a>
+                </div>
+            </div>
+        </div>
+
+        <!--/row-->
+    </div><!--/container-->
+    <!--=== End Content Part ===-->
+
+<?php include('footer_inc.php'); ?>
